f09565be92dd14aea588fc16660bd0041c2032be
Roman Avdeev <RomanAAvdeev@proton.me>
2024-11-04 19:53:43 +0400

    Add padding to sort icons

--- lib/features/tasks/presentation/widgets/task_sort.dart
+++ lib/features/tasks/presentation/widgets/task_sort.dart
@@ -16,58 +16,61 @@ class _TaskSortState extends State<TaskSort> {
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButtonHideUnderline(
-      child: DropdownButton2<SortTasksBy>(
-          customButton: widget.icon,
-          dropdownStyleData: DropdownStyleData(
-            width: 160,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(4),
-              color: Colors.grey[900],
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton2<SortTasksBy>(
+            customButton: widget.icon,
+            dropdownStyleData: DropdownStyleData(
+              width: 160,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                color: Colors.grey[900],
+              ),
             ),
-          ),
-          items: const [
-            DropdownMenuItem<SortTasksBy>(
-                value: SortTasksBy.id,
-                child: Text(
-                  "id",
-                  style: TextStyle(color: Colors.white),
-                )),
-            DropdownMenuItem<SortTasksBy>(
-                value: SortTasksBy.name,
-                child: Text(
-                  "name",
-                  style: TextStyle(color: Colors.white),
-                )),
-            DropdownMenuItem<SortTasksBy>(
-                value: SortTasksBy.priority,
-                child: Text(
-                  "priority",
-                  style: TextStyle(color: Colors.white),
-                )),
-            DropdownMenuItem<SortTasksBy>(
-                value: SortTasksBy.urgency,
-                child: Text(
-                  "urgency",
-                  style: TextStyle(color: Colors.white),
-                )),
-            DropdownMenuItem<SortTasksBy>(
-                value: SortTasksBy.eta,
-                child: Text(
-                  "eta",
-                  style: TextStyle(color: Colors.white),
-                )),
-          ],
-          onChanged: (value) {
-            setState(() {
-              if (value != null && sorting != value) {
-                sorting = value;
-                if (widget.onChanged != null) {
-                  widget.onChanged!(sorting);
+            items: const [
+              DropdownMenuItem<SortTasksBy>(
+                  value: SortTasksBy.id,
+                  child: Text(
+                    "id",
+                    style: TextStyle(color: Colors.white),
+                  )),
+              DropdownMenuItem<SortTasksBy>(
+                  value: SortTasksBy.name,
+                  child: Text(
+                    "name",
+                    style: TextStyle(color: Colors.white),
+                  )),
+              DropdownMenuItem<SortTasksBy>(
+                  value: SortTasksBy.priority,
+                  child: Text(
+                    "priority",
+                    style: TextStyle(color: Colors.white),
+                  )),
+              DropdownMenuItem<SortTasksBy>(
+                  value: SortTasksBy.urgency,
+                  child: Text(
+                    "urgency",
+                    style: TextStyle(color: Colors.white),
+                  )),
+              DropdownMenuItem<SortTasksBy>(
+                  value: SortTasksBy.eta,
+                  child: Text(
+                    "eta",
+                    style: TextStyle(color: Colors.white),
+                  )),
+            ],
+            onChanged: (value) {
+              setState(() {
+                if (value != null && sorting != value) {
+                  sorting = value;
+                  if (widget.onChanged != null) {
+                    widget.onChanged!(sorting);
+                  }
                 }
-              }
-            });
-          }),
+              });
+            }),
+      ),
     );
   }
 }
