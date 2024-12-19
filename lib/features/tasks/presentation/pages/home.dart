8fe34b115ae2543e608b472784993d5fcfb747aa
Roman Avdeev <RomanAAvdeev@proton.me>
2024-12-19 16:32:26 +0400

    Changed async to be clearer

--- lib/features/tasks/presentation/pages/home.dart
+++ lib/features/tasks/presentation/pages/home.dart
@@ -20,8 +20,7 @@ class _HomeState extends State<Home> {
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) async {
-      var newTasks = await getAllTasks();
+    getAllTasks().then((newTasks) {
       setState(() {
         tasks = newTasks;
       });
